822d73a173d2f64f3bb5c7e69cb850f1d84c9a46
YahyaAppec <nick@appec.nl>
2026-02-01 22:06:34 +0200

    chore: add prettier and eslint confugrations into eslint mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,43 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      prettier,
+      tailwindcss,
+    },
+    rules: {
+      // Prettier
+      'prettier/prettier': 'error',
+
+      // Tailwind
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+
+      // Optional but useful in Next 16
+      '@next/next/no-html-link-for-pages': 'off', // App Router safe
+    },
+  },
+
+  // Disable conflicting formatting rules
+  eslintConfigPrettier,
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
