861fd95633f7bb166e11378d6379605416e1e42f
SilverFang <silverfang@YahyaPros-MacBook-Pro.local>
2026-02-10 12:08:07 +0200

    feat: add root layout + pages routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next"
-import "./globals.css"
-import { ReactNode } from "react"
-import localFont from "next/dist/compiled/@next/font/dist/local"
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import type { ReactNode } from "react";
+
+import "./globals.css";
 
 const ibmBlexSans = localFont({
   src: [
@@ -26,24 +27,29 @@ const ibmBlexSans = localFont({
       style: "normal",
     },
   ],
-})
+});
 
 const bebasNeue = localFont({
-  src: [{ path: "./fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" }],
+  src: [
+    { path: "./fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
   variable: "--bebas-neue",
-})
+});
 
 export const metadata: Metadata = {
   title: "Book Knowledge",
-  description: "book knowledge is a book borrowing university library management solution",
-}
+  description:
+    "book knowledge is a book borrowing university library management solution",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${ibmBlexSans.className} ${bebasNeue.variable} antialiased`}>
+      <body
+        className={`${ibmBlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
-  )
+  );
 }
