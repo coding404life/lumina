efea16c6025c986a3a2c429e6692f7b054272bee
YahyaPro <kingwar66@gmail.com>
2026-03-02 17:41:15 +0200

    refactor: Admin home page now directly displays the admin users page content.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,1 @@
-import { redirect } from "next/navigation";
-
-const AdminHomePage = () => {
-  redirect("/admin/users");
-};
-
-export default AdminHomePage;
+export { default } from "./users/page";
